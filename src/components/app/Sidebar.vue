09d3652c9ffe32c14aac36de0916ff7ed52dfd44
mark88coder <mkuchenash@gmail.com>
2021-04-04 20:29:54 +0300

    Edited main layout

--- src/components/app/Sidebar.vue
+++ src/components/app/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="sidenav app-sidenav" :class="{open: value}">
+  <ul class="sidenav app-sidenav" :class="{ open: value }">
     <!-- Динамическая генерация ссылок (перход при клике) при помощи компонента router-link + Подсвечивание активных ссылок -->
     <router-link
       v-for="link in links"
@@ -9,22 +9,22 @@
       :to="link.url"
       :exact="link.exact"
     >
-      <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}</a>
     </router-link>
   </ul>
 </template>
 
 <script>
 export default {
-  props: ['value'],
+  props: ["value"],
   data: () => ({
     links: [
-      {title: 'Счет', url: '/', exact: true},
-      {title: 'История', url: '/history'},
-      {title: 'Планирование', url: '/planning'},
-      {title: 'Новая запись', url: '/record'},
-      {title: 'Категории', url: '/categories'}
-    ]
-  })
-}
+      { title: "Счет", url: "/", exact: true },
+      { title: "История", url: "/history" },
+      { title: "Планирование", url: "/planning" },
+      { title: "Новая запись", url: "/record" },
+      { title: "Категории", url: "/categories" },
+    ],
+  }),
+};
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="app-main-layout">
-    
     <Navbar @click="isOpen = !isOpen" />
 
     <Sidebar v-model="isOpen" />
-    
-    <main class="app-content" :class="{full: !isOpen}">
+
+    <main class="app-content" :class="{ full: !isOpen }">
       <div class="app-page">
         <router-view />
       </div>
     </main>
 
     <div class="fixed-action-btn">
-      <a class="btn-floating btn-large blue" href="#">
+      <router-link class="btn-floating btn-large blue" to="/record">
         <i class="large material-icons">add</i>
-      </a>
+      </router-link>
     </div>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/app/Navbar'
-import Sidebar from '@/components/app/Sidebar'
+import Navbar from "@/components/app/Navbar";
+import Sidebar from "@/components/app/Sidebar";
 
 export default {
-  name: 'main-layout',
+  name: "main-layout",
   data: () => ({
-    isOpen: true
+    isOpen: true,
   }),
   components: {
-    Navbar, Sidebar
-  }
-}
+    Navbar,
+    Sidebar,
+  },
+};
 </script>
